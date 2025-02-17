db0d09e20bc76c66d8e33480603f1ed3b0153eaf
DreamHight <DreamHight@users.noreply.github.com>
2025-02-17 02:30:02 +0000

    deploy: 8534b92126a4633f01c49aab9d94e79485d43989

---
+++ assets/interceptor-legacy-DPBIFGll.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy-CHI2HpQ2.js"],(function(e,t){"use strict";var n,c;return{setters:[e=>{n=e.aL,c=e.a_}],execute:function(){e("c",(function(e,{args:t=[],done:r,canceled:s,error:a}){if(e){const i=e.apply(null,t);n(i)?i.then((e=>{e?r():s&&s()})).catch(a||c):i?r():s&&s()}else r()}))}}}));
